a6bd62e5c4e4ab722fc099ad67687e1bf233864e
Ethan Freeman <ethanfree23@gmail.com>
2022-01-16 12:04:44 -0600

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+
+
+
+function change(event) {
+    event.preventDefault();
+    console.log("Entering password...")
+}
+
+function Keypad() {
+    return (
+        <>
+            <form onChange={change}>
+                <input type="password"></input>
+            </form>
+        </>
+    )
+}
+
+export default Keypad;
